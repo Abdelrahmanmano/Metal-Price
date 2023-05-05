aad9c74b2c9ab00921acfb52cef7a26dc4183035
Abdelrhman Ashraf <abdelrhman.ashraf@itqansystems.com>
2023-05-06 01:32:08 +0300

    [ADD] metal price

--- metal_price/models/account_move_line.py
+++ metal_price/models/account_move_line.py
@@ -9,10 +9,18 @@ class AccountMoveLine(models.Model):
 
     uom_category_id = fields.Many2one('uom.category', related='product_uom_id.category_id')
     price_subtotal = fields.Monetary(
-        inverse='_inverse_subtotals'
+        string='Subtotal',
+        compute='_compute_totals',
+        inverse='_inverse_subtotals',
+        store=True,
+        currency_field='currency_id',
     )
     price_total = fields.Monetary(
-        inverse='_inverse_totals'
+        string='Total',
+        compute='_compute_totals',
+        inverse='_inverse_totals',
+        store=True,
+        currency_field='currency_id',
     )
 
     @api.depends('price_total')
@@ -24,30 +32,6 @@ class AccountMoveLine(models.Model):
     @api.depends('price_subtotal')
     def _inverse_subtotals(self):
         self.price_unit = self.price_subtotal / self.quantity
-
-    @api.depends('quantity', 'discount', 'price_unit', 'tax_ids', 'currency_id')
-    def _compute_totals(self):
-        for line in self:
-            if line.display_type != 'product':
-                line.price_total = line.price_subtotal = False
-            # Compute 'price_subtotal'.
-            line_discount_price_unit = line.price_unit * (1 - (line.discount / 100.0))
-            subtotal = line.quantity * line_discount_price_unit
-
-            # Compute 'price_total'.
-            if line.tax_ids:
-                taxes_res = line.tax_ids.compute_all(
-                    line_discount_price_unit,
-                    quantity=line.quantity,
-                    currency=line.currency_id,
-                    product=line.product_id,
-                    partner=line.partner_id,
-                    is_refund=line.is_refund,
-                )
-                line.price_subtotal = taxes_res['total_excluded']
-                line.price_total = taxes_res['total_included']
-            else:
-                line.price_total = line.price_subtotal = subtotal
 
     @api.onchange('price_subtotal', 'price_total', 'quantity')
     def _onchange_subtotal(self):
