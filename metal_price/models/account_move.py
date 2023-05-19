25c93f97e3fe9e1ed20495715565c8d24da1644a
Abdelrhman Ashraf <abdelrhmanmano7@gmail.com>
2023-05-20 02:06:45 +0300

    ADD

--- metal_price/models/account_move.py
+++ metal_price/models/account_move.py
@@ -21,3 +21,28 @@ class AccountMove(models.Model):
         for rec in self:
             rec.have_weight_uom = any(
                 line.uom_category_id == self.env.ref('uom.product_uom_categ_kgm') for line in rec.invoice_line_ids)
+
+    @api.depends(
+        'line_ids.matched_debit_ids.debit_move_id.move_id.payment_id.is_matched',
+        'line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual',
+        'line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual_currency',
+        'line_ids.matched_credit_ids.credit_move_id.move_id.payment_id.is_matched',
+        'line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual',
+        'line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual_currency',
+        'line_ids.balance',
+        'line_ids.currency_id',
+        'line_ids.amount_currency',
+        'line_ids.amount_residual',
+        'line_ids.amount_residual_currency',
+        'line_ids.payment_id.state',
+        'line_ids.full_reconcile_id')
+    def _compute_amount(self):
+        if self.env.context.get('from_onchange', False):
+            return
+        super(AccountMove, self)._compute_amount()
+        for move in self:
+            move.amount_untaxed = sum(move.line_ids.mapped('price_subtotal'))
+            move.amount_tax = sum(move.line_ids.mapped('price_total')) - sum(move.line_ids.mapped('price_subtotal'))
+            move.amount_total = sum(move.line_ids.mapped('price_total'))
+            move.amount_residual = sum(move.line_ids.mapped('price_total'))
+
--- metal_price/models/account_move_line.py
+++ metal_price/models/account_move_line.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
+import math
 
 
 class AccountMoveLine(models.Model):
@@ -23,15 +24,44 @@ class AccountMoveLine(models.Model):
         currency_field='currency_id',
     )
 
-    @api.depends('price_total')
-    def _inverse_totals(self):
-        tax_amount = sum(tax.amount for tax in self.tax_ids)
-        self.price_subtotal = self.price_total * 100 / (100 + tax_amount)
-        self.price_unit = self.price_subtotal / self.quantity
+    @api.depends('price_unit', 'quantity', 'discount', 'tax_ids')
+    def _compute_totals(self):
+        if self.env.context.get('from_onchange', False):
+            return
+        for line in self:
+            taxes = line.tax_ids.compute_all(line.price_unit * (1 - (line.discount or 0.0) / 100.0),
+                                             line.move_id.currency_id, line.quantity, product=line.product_id,
+                                             partner=line.move_id.partner_id)
+            line.update({
+                'price_subtotal': taxes['total_excluded'],
+                'price_total': taxes['total_included'],
+            })
 
-    @api.depends('price_subtotal')
+    @api.onchange('price_total')
+    def _inverse_totals(self):
+        new_context = self.env.context.copy()
+        new_context['from_onchange'] = True
+        self.env.context = new_context
+        tax_amount = sum(tax.amount for tax in self.tax_ids)
+        self.with_context(from_onchange=True).price_subtotal = self.price_total * 100 / (100 + tax_amount)
+        self.with_context(from_onchange=True).price_unit = self.price_subtotal / self.quantity
+        self.with_context(from_onchange=True).move_id._compute_amount()
+
+    @api.onchange('price_subtotal')
     def _inverse_subtotals(self):
-        self.price_unit = self.price_subtotal / self.quantity
+        new_context = self.env.context.copy()
+        new_context['from_onchange'] = True
+        self.env.context = new_context
+        tax_amount = sum(tax.amount for tax in self.tax_ids)
+        self.with_context(from_onchange=True).price_unit = self.price_subtotal / self.quantity
+        self.with_context(from_onchange=True).move_id._compute_amount()
+
+    @api.constrains('quantity', 'product_id')
+    def check_quantity(self):
+        category = self.env.ref('uom.product_uom_categ_kgm')
+        for rec in self:
+            if rec.quantity <= 0 and rec.uom_category_id == category:
+                raise ValidationError("Quantity should be positive")
 
     @api.onchange('price_subtotal', 'price_total', 'quantity')
     def _onchange_subtotal(self):
@@ -46,9 +76,5 @@ class AccountMoveLine(models.Model):
             }
             return warning
 
-    @api.constrains('quantity', 'product_id')
-    def check_quantity(self):
-        category = self.env.ref('uom.product_uom_categ_kgm')
-        for rec in self:
-            if rec.quantity <= 0 and rec.uom_category_id == category:
-                raise ValidationError("Quantity should be positive")
+    def truncate(self, f, n):
+        return math.ceil(f * 10 ** n) / 10 ** n
